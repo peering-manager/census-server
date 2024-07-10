ab01aca08caa339ab5f32f9681ba65e790639c29
Guillaume Mazoyer <guillaume@mazoyer.eu>
2024-07-11 00:28:53 +0200

    Do not lookup country for private IP

--- backend/census_api/utils.py
+++ backend/census_api/utils.py
@@ -1,3 +1,4 @@
+import ipaddress
 import logging
 
 import httpx
@@ -8,6 +9,10 @@ from .core.config import settings
 async def resolve_country_for_ip(*, ip_address: str) -> str | None:
     if not settings.IPINFO_TOKEN:
         logging.error("cannot use ipinfo lookup, IPINFO_TOKEN not set")
+        return None
+
+    address = ipaddress.ip_address(address=ip_address)
+    if address.is_private:
         return None
 
     async with httpx.AsyncClient() as client:
