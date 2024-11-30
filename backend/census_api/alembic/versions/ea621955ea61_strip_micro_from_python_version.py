f8932fd6e2a249607254bba1bda98ba596fd3d9b
Guillaume Mazoyer <guillaume@mazoyer.eu>
2024-11-30 23:36:07 +0100

    Add migration

---
+++ backend/census_api/alembic/versions/ea621955ea61_strip_micro_from_python_version.py
@@ -0,0 +1,32 @@
+"""Strip micro from Python version
+
+Revision ID: ea621955ea61
+Revises: 91c2a580f3c6
+Create Date: 2024-11-29 22:34:12.724662
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = "ea621955ea61"
+down_revision: Union[str, None] = "91c2a580f3c6"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute(
+        """
+        UPDATE "censusrecord"
+        SET "python_version" = regexp_replace("python_version", '^(\d+\.\d+)\..*$', '\1')
+        WHERE "python_version" ~ '^(\d+\.\d+)\..*$'
+        """
+    )
+
+
+def downgrade() -> None:
+    pass
