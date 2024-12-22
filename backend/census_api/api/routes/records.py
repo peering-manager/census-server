1a86c92baceafecce7c469bef9dc66c63382cf4d
Guillaume Mazoyer <guillaume@mazoyer.eu>
2024-12-22 22:07:40 +0100

    Always update records but limit notifications

--- backend/census_api/api/routes/records.py
+++ backend/census_api/api/routes/records.py
@@ -39,16 +39,21 @@ async def create_record(
     db_record = results.first()
 
     event: CensusRecordEvent | None = None
+    send_notification = True
+
     if db_record:
         interval = (
             now - db_record.updated_at.astimezone(tz=timezone.utc)
         ).total_seconds()
         python_version = version_strip_micro(version=record.python_version)
 
-        if (
-            db_record.version != record.version
-            or db_record.python_version != python_version
-        ) and interval >= settings.RATE_LIMIT:
+        if interval >= settings.RATE_LIMIT:
+            # Only send a notification if something has really changed
+            send_notification = (
+                db_record.version != record.version
+                or db_record.python_version != python_version
+            )
+
             db_record.version = record.version
             db_record.python_version = python_version
             db_record.updated_at = now
@@ -71,7 +76,8 @@ async def create_record(
         await session.commit()
         await session.refresh(db_record)
 
-        await discord_notify(event=event, record=db_record)
+        if send_notification:
+            await discord_notify(event=event, record=db_record)
 
     return db_record
 
